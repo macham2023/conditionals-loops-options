d2264842bf885a3985106e56ebae536374597ce3
macham2023 <machamisaac079@gmail.com>
2025-08-19 20:28:48 +0100

    real

--- task.py
+++ task.py
@@ -1,82 +1,78 @@
 
+count = 0
+balance = 1000
+code = "*556#"
+
+
 print(
 '''
 |==========================|
 |  ++++++++++              |
 |WELCOME! TO MTN SERVICES! |
 |                          |
+| Everywhere you go!       |  
+|    ++++++++++            |
 |                          |
-|    ++++++++++            |    
 |==========================|
  '''
 )
-print("Enter *556# to continue")
-
-balance = 1000
-count = 0
-code = "*556#"
-
-while True:
-    user = input("Enter the pin here:")
-    if user == code:
-        print("")
+user = input("Enter the pin here:")
+if user == code:
+    while True:
+        #user = input("Enter the pin here:")
         print(
         '''
-        AVAILABLE SERVICES
-        1 :Buy Airtime
-        2 :Buy Data
-        3 :Borrow
-        4 :Check Balance
-        5 :Stop
+	AVAILABLE SERVICES
+	1 :Buy Airtime
+	2 :Buy Data
+	3 :Borrow AIrtime/Data
+	4 :Check Balance
+	5 :Stop
         ''')
         user_option = int(input("Enter option:"))
         if user_option == 1:
             print("Enjoy our airtime bundle at a minimum rate")
             print("")
             user = float(input("Enter the amount you wish to buy:₦"))
+	 
             if balance >= user:
-                balance -= user
+                balance -= user 
                 print(f"you have been credited with : ₦{user}")
             else:
                 print("Insufficient funds!")
         elif user_option == 2:
             print(
-            '''
-            Buy our cheap Data Bundles
-            1 :500MB = ₦200
-            2 :700MB = ₦250
-            3 :1GB = ₦300
+	    '''
+	    Buy our cheap Data Bundles
+	    1 :500MB = ₦200
+	    2 :700MB = ₦250
+	    3 :1GB = ₦300
             4 :2GB = ₦600
             5 :3GB = ₦900
-            ''')
-            user = int(input("Enter your Data plan:"))
-            if balance >= user:
-                if user == 1:
-                    balance -= user
-                    print("succesfully recieved 500MB")
-                elif user == 2:
-                    balance -= user
-                    print("successfully credited with 1GB")
-                elif user == 3:
-                    balance -= user
-                    print("successfully recieved 2GB!")
-                elif user == 4:
-                    balance -= user
-                    print("successfully recieved 3GB!")
-                elif user == 5:
-                    balance -= user
-                    print("successfully recieved 3.5GB!")
-                else:
-                    print("Invalid pin!")
-                
-                print("")
-               
+            '''
+	    )
+            user = int(input("Enter option:"))
+            if user == 1:
+                balance -= user
+                print("succesfully recieved 500MB")
+            elif user == 2:
+                balance -= user
+                print("successfully credited with 1GB")
+            elif user == 3:
+                balance -= user
+                print("successfully recieved 2GB!")
+            elif user == 4:
+                balance -= user
+                print("successfully recieved 3GB!")
+            elif user == 5:
+                balance -= user
+                print("successfully recieved 3.5GB!")
             else:
-                print("Invalid")
+                print("Invalid command")
             
         elif user_option == 3:
-            print(
-            '''
+	    
+            print('''
             ENTER OPTION:
             1.Borrow Airtime
             2.Borrow Data
@@ -84,6 +80,7 @@ while True:
             user = int(input("Choose option:"))
             print("")
             if user == 1:
+                
                 print("Enjoy cheap airtime as low as 15% rate")
                 user = float(input("Enter your amount:"))
                 if balance >= user:
@@ -93,7 +90,7 @@ while True:
                     print(f"you account have been credited with:  ₦{user}")
                     print(f"Remaining balance is : {balance}")
                 else:
-                    print("Invalid amount")
+                    print("Insufficient funds")
             elif user == 2:
                 print("Borrow Data as low as %15 rate ")
                 print('''
@@ -138,21 +135,21 @@ while True:
                     balance -= percent
                     print("Suceesfully recieved: 3GBG")
                     print(f"remaining balance is {balance}")
-
+                
                 else :
-                    print("Invalid score")
-
+                    print("Invalid option")
             else:
-                print("Invalid Option")
+                print("Invalid option")
+
         elif user_option == 4:
             print(f"Your balance is: {balance}")
         elif user_option == 5:
-            print("trannsaction stopped!")
+            print("transaction stopped!")
             break
         else:
             print("Invalid option")
            
-       
-    else:
-        print("Invalid pin")
-count += 1
+else:
+    print("Invalid pin")
+
+
